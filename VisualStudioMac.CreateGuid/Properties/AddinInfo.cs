c9ee174137b3a969bf16701b3bd30cec7715bea6
Adam M <itadam@gmail.com>
2023-04-28 21:23:45 -0700

    Declared version number and added AddinInfo file

---
+++ VisualStudioMac.CreateGuid/Properties/AddinInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "VisualStudioMac.CreateGuid",
+    Namespace = "VisualStudioMac.CreateGuid",
+    Version = "0.0.1"
+)]
+
+[assembly: AddinName("Create Guid Addin")]
+[assembly: AddinCategory("Utility")]
+[assembly: AddinDescription("Create Guid Addin for Visual Studio for Mac")]
+[assembly: AddinAuthor("Adam Mitchell")]
+[assembly: AddinUrl("https://github.com/itadam/VS4Mac-CreateGuid")]
+
+[assembly: AddinDependency("::MonoDevelop.Core", BuildInfo.Version)]
+[assembly: AddinDependency("::MonoDevelop.Ide", BuildInfo.Version)]
